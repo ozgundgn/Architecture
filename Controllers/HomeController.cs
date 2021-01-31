c7de6d3f2efbf1dfcd21820ba3b9aa58363376f2
Özgün DOĞAN <ozgun.dogan@prestijyazilim.com.tr>
2021-02-01 00:10:20 +0300

    active link eklendi.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,11 +16,13 @@ namespace Architecture.Controllers
         [ExceptionFilter]
         public ActionResult Index()
         {
+            ViewBag.ActiveLink = "Index";
             return View();
         }
 
         public ActionResult About()
         {
+            ViewBag.ActiveLink = "About";
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -28,34 +30,46 @@ namespace Architecture.Controllers
 
         public ActionResult Contact()
         {
+            ViewBag.ActiveLink = "Contact";
+
             return View();
         }
         public ActionResult References()
         {
+            ViewBag.ActiveLink = "References";
+
             return View();
         }
         public ActionResult IcMimari()
         {
+            ViewBag.ActiveLink = "IcMimari";
+
             return View();
         }  
         public ActionResult MimariRuhsat()
         {
+            ViewBag.ActiveLink = "MimariRuhsat";
+
             return View();
         } 
         public ActionResult Restorasyon()
         {
+            ViewBag.ActiveLink = "Restorasyon";
             return View();
         }
         public ActionResult AhsapKutuk()
         {
+            ViewBag.ActiveLink = "AhsapKutuk";
             return View();
         }
         public ActionResult Designs()
         {
+            ViewBag.ActiveLink = "Designs";
             return View();
         }
         public ActionResult Media()
         {
+            ViewBag.ActiveLink = "Media";
             return View();
         }
         public ActionResult Error()
